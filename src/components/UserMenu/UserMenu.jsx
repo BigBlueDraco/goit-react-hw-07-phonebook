91f58741bb33f60f8911780a4e18285f2174bc95
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-11-04 00:30:42 +0200

    add styled to nav bar

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,19 +1,23 @@
-import { Button } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
 import { selectUser } from 'redux/auth/selectors';
+import { Button } from 'components/Button/Button';
 import s from './UserMenu.module.scss';
+import { StyledNavLink } from 'components/StyledNavLinks/StyledNavLink';
+import { NavLink } from 'react-router-dom';
 
 export const UserMenu = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
   return (
-    <Button
-      className={s['logout']}
-      type="button"
-      onClick={() => dispatch(logOut())}
-    >
-      Logout
-    </Button>
+    <div className={s['logout']}>
+      <Button
+        className={s['logout__btn']}
+        type="button"
+        onClick={() => dispatch(logOut())}
+      >
+        <NavLink to="/">Log Out</NavLink>
+      </Button>
+    </div>
   );
 };
