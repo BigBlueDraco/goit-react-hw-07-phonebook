d2b3b8559f03473f7c1b40402f81a1ad365e60f7
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-11-05 20:32:13 +0200

    fix navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,12 +4,9 @@ import { selectIsLoggedIn } from 'redux/auth/selectors';
 import s from './navigation.module.scss';
 
 export const Navigation = () => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
   return (
     <nav className={s['navgation']}>
       <NavLink to="/">PhonebookApp</NavLink>
-      {/* {isLoggedIn && <NavLink to="/phonebook">Phonebook</NavLink>} */}
     </nav>
   );
 };
